9d068d4ccee55072e0bbc089fd9d4a4532beeab3
simenpf <simen.flotaker@gmail.com>
2021-06-07 15:33:30 +0100

    merged

--- clock.py
+++ clock.py
@@ -1,4 +1,5 @@
 from guizero import App, Drawing
+from datetime import datetime
 import time
 
 w=1280
@@ -86,6 +87,21 @@ def write(word,c):
 def write_sentence(words,c):
         for word in words:
                 write(word,c)
+
+def write_time():
+    now = datetime.now()
+    hour = now.hour()
+    minute = now.minute()
+    if(hour>12):
+        print("pm")
+    else:
+        print("am")
+    print("it is")
+
+    if(minute < 35):
+        print("past")
+    else:
+        print("to")
 
 write_sentence(["it","is","twenty","five1","past","three"],"white")
 write("am","yellow")
