658f9ac216b6fd354ffc8f2bf75cf08f39d98437
Simen Piene Fløtaker <Simen.flotaker@gmail.com>
2021-06-11 07:21:43 +0200

    Picture working with letters, but not logo and minutes

--- tkintertest.py
+++ tkintertest.py
@@ -111,7 +111,7 @@ def set_minutes(m,c):
 
 def set_letter(x,y,c):
         canvas.itemconfig(letter_rects[x][y], fill=c)
-def set_letter_img(x,y):
+def set_letter_img(x,y,c):
         set_letter(x,y,"#ABABAB")
         canvas.create_image(0,0, anchor=NW, image=img_tk)
         for x in range(11):
@@ -125,72 +125,68 @@ def set_letter_img(x,y):
                                 letter_rects[x].append(canvas.create_rectangle(x0, y0, x1, y1, fill=curr_color))
                                 
 
-def set_letters(letters,c):
+def set_letters(letters,c,set_letter):
         for l in letters:
                 set_letter(l[0],l[1],c)
-def set_letters_img(letters,c):
-        for l in letters:
-                set_letter_img(l[0],l[1],c)
-def set_all_letters(c):
+
+def set_all_letters(c,set_letter):
         for x in range(11):
                 for y in range(10):
                         set_letter(x,y,c)
-def set_all_letters_img(c):
-        canvas.create_image(0,0, anchor=NW, image=img_tk) 
-def write(word,c):
+def write(word,c,set_letter):
                 if word == "it":
-                        set_letters([[0,0],[1,0]],c)
+                        set_letters([[0,0],[1,0]],c,set_letter)
                 elif word == "is":
-                        set_letters([[3,0],[4,0]],c)
+                        set_letters([[3,0],[4,0]],c,set_letter)
                 elif word == "am":
-                        set_letters([[7,0],[8,0]],c)                   
+                        set_letters([[7,0],[8,0]],c,set_letter)                   
                 elif word == "pm":
-                        set_letters([[9,0],[10,0]],c)
+                        set_letters([[9,0],[10,0]],c,set_letter)
                 elif word == "a":
-                        set_letters([[0,1]],c)
+                        set_letters([[0,1]],c,set_letter)
                 elif word == "quarter":
-                        set_letters([[2,1],[3,1],[4,1],[5,1],[6,1],[7,1],[8,1]],c)
+                        set_letters([[2,1],[3,1],[4,1],[5,1],[6,1],[7,1],[8,1]],c,set_letter)
                 elif word == "twenty":
-                        set_letters([[0,2],[1,2],[2,2],[3,2],[4,2],[5,2]],c)
+                        set_letters([[0,2],[1,2],[2,2],[3,2],[4,2],[5,2]],c,set_letter)
                 elif word == "five1":
-                        set_letters([[6,2],[7,2],[8,2],[9,2]],c)
+                        set_letters([[6,2],[7,2],[8,2],[9,2]],c,set_letter)
                 elif word == "half":
-                        set_letters([[0,3],[1,3],[2,3],[3,3]],c)
+                        set_letters([[0,3],[1,3],[2,3],[3,3]],c,set_letter)
                 elif word == "ten1":
-                        set_letters([[5,3],[6,3],[7,3]],c)
+                        set_letters([[5,3],[6,3],[7,3]],c,set_letter)
                 elif word == "to":
-                        set_letters([[9,3],[10,3]],c)
+                        set_letters([[9,3],[10,3]],c,set_letter)
                 elif word == "past":
-                        set_letters([[0,4],[1,4],[2,4],[3,4]],c)
+                        set_letters([[0,4],[1,4],[2,4],[3,4]],c,set_letter)
                 elif word == "one":
-                        set_letters([[0,5],[1,5],[2,5]],c)
+                        set_letters([[0,5],[1,5],[2,5]],c,set_letter)
                 elif word == "two":
-                        set_letters([[8,6],[9,6],[10,6]],c)
+                        set_letters([[8,6],[9,6],[10,6]],c,set_letter)
                 elif word == "three":
-                        set_letters([[6,5],[7,5],[8,5],[9,5],[10,5]],c)
+                        set_letters([[6,5],[7,5],[8,5],[9,5],[10,5]],c,set_letter)
                 elif word == "four":
-                        set_letters([[0,6],[1,6],[2,6],[3,6]],c)
+                        set_letters([[0,6],[1,6],[2,6],[3,6]],c,set_letter)
                 elif word == "five":
-                        set_letters([[4,6],[5,6],[6,6],[7,6]],c)
+                        set_letters([[4,6],[5,6],[6,6],[7,6]],c,set_letter)
                 elif word == "six":
-                        set_letters([[3,5],[4,5],[5,5]],c)
+                        set_letters([[3,5],[4,5],[5,5]],c,set_letter)
                 elif word == "seven":
-                        set_letters([[0,8],[1,8],[2,8],[3,8],[4,8]],c)
+                        set_letters([[0,8],[1,8],[2,8],[3,8],[4,8]],c,set_letter)
                 elif word == "eight":
-                        set_letters([[0,7],[1,7],[2,7],[3,7],[4,7]],c)
+                        set_letters([[0,7],[1,7],[2,7],[3,7],[4,7]],c,set_letter)
                 elif word == "nine":
-                        set_letters([[7,4],[8,4],[9,4],[10,4]],c)
+                        set_letters([[7,4],[8,4],[9,4],[10,4]],c,set_letter)
                 elif word == "ten":
-                        set_letters([[0,9],[1,9],[2,9]],c)
+                        set_letters([[0,9],[1,9],[2,9]],c,set_letter)
                 elif word == "eleven":
-                        set_letters([[5,7],[6,7],[7,7],[8,7],[9,7],[10,7]],c)
+                        set_letters([[5,7],[6,7],[7,7],[8,7],[9,7],[10,7]],c,set_letter)
                 elif word == "twelve":
-                        set_letters([[5,8],[6,8],[7,8],[8,8],[9,8],[10,8]],c)
-def write_sentence(words,c):
+                        set_letters([[5,8],[6,8],[7,8],[8,8],[9,8],[10,8]],c,set_letter)
+def write_sentence(words,c,set_letter):
         for word in words:
-                write(word,c)
+                write(word,c,set_letter)
 
-def write_time(c,c2):
+def write_time(c,c2,set_letter):
     now = datetime.now()
     hour = now.hour
     minute = now.minute
@@ -201,59 +197,59 @@ def write_time(c,c2):
 
 
     # It is
-    write("it",c)
-    write("is",c)
+    write("it",c,set_letter)
+    write("is",c,set_letter)
 
     # five, ten, quarter, twenty, twentyfive, half
     minute_block = minute // 5
     if minute_block == 0:
         pass
     elif minute_block == 1 or minute_block == 11:
-        write("five1",c)
+        write("five1",c,set_letter)
     elif minute_block == 2 or minute_block == 10:
-        write("ten1",c)
+        write("ten1",c,set_letter)
     elif minute_block == 3 or minute_block == 9:
-        write("quarter",c)
+        write("quarter",c,set_letter)
     elif minute_block == 4 or minute_block == 8:
-        write("twenty",c)
+        write("twenty",c,set_letter)
     elif minute_block == 5 or minute_block == 7:
-        write_sentence(["twenty","five1"],c)
+        write_sentence(["twenty","five1"],c,set_letter)
     elif minute_block == 6:
-        write("half",c)
+        write("half",c,set_letter)
 
     # Past, to or whole hour
     if(minute >= 35 ):
-        write("to",c)
+        write("to",c,set_letter)
         hour+=1
     elif(minute >= 5):
-        write("past",c)
+        write("past",c,set_letter)
         
 
     # one, two, three, ...
     if (hour == 1) or (hour == 13):
-        write("one",c)
+        write("one",c,set_letter)
     elif (hour == 2) or (hour == 14):
-        write("two",c)
+        write("two",c,set_letter)
     elif (hour == 3) or (hour == 15):
-        write("three",c)
+        write("three",c,set_letter)
     elif (hour == 4) or (hour == 16):
-        write("four",c)
+        write("four",c,set_letter)
     elif (hour == 5) or (hour == 17):
-        write("five",c)
+        write("five",c,set_letter)
     elif (hour == 6) or (hour == 18):
-        write("six",c)
+        write("six",c,set_letter)
     elif (hour == 7) or (hour == 19):
-        write("seven",c)
+        write("seven",c,set_letter)
     elif (hour == 8) or (hour == 20):
-        write("eight",c)
+        write("eight",c,set_letter)
     elif (hour == 9) or (hour == 21):
-        write("nine",c)
+        write("nine",c,set_letter)
     elif (hour == 10) or (hour == 22):
-        write("ten",c)
+        write("ten",c,set_letter)
     elif (hour == 11) or (hour == 23):
-        write("eleven",c)
+        write("eleven",c,set_letter)
     elif (hour == 12) or (hour == 24):
-        write("twelwe",c)
+        write("twelwe",c,set_letter)
 
     # Extra minutes
     surplus_minutes = minute % 5
@@ -276,7 +272,7 @@ def write_time(c,c2):
          elif surplus_minutes == 4:
              set_minutes(1,c2)
 
-def write_am_pm(c_am,c_pm):
+def write_am_pm(c_am,c_pm,set_letter):
     now = datetime.now()
     hour = now.hour
     minute = now.minute
@@ -287,17 +283,16 @@ def write_am_pm(c_am,c_pm):
 
     # AM or PM
     if(hour>12):
-        write("pm",c_pm)
+        write("pm",c_pm,set_letter)
     else:
-        write("am",c_am)
+        write("am",c_am,set_letter)
 def clear_clock():
-     set_all_letters("black")
+     set_all_letters("black",set_letter)
      set_minutes(4,"black")
 def update_clock():
-     canvas.create_image(0,0, anchor=NW, image=img_tk) 
      clear_clock()
-     write_time("white","red")
-     write_am_pm("#909090","#707070")
+     write_time("white","red",set_letter_img)
+     write_am_pm("#909090","#707070",set_letter)
      sec = datetime.now().second
      if sec%2:
          set_logo("green")
@@ -305,10 +300,8 @@ def update_clock():
          set_logo("blue")
      root.after(1000, update_clock)
 
-set_all_letters("black")
-set_letter(5,5,"blue")
-set_letter_img(0,1)
-#root.after(1000,update_clock)
+
+root.after(1000,update_clock)
 
 root.mainloop()
 
